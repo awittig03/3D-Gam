510af75b0307e79540a5b85c80030be11f4c82c7
awittig03 <143130984+awittig03@users.noreply.github.com>
2023-12-10 16:32:28 -0500

    added skina and more

--- Player.gd
+++ Player.gd
@@ -3,23 +3,36 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SCROLL_SPEED = 1.0
+const MOUSE_SENSITIVITY = 0.002
+const MOUSE_RANGE = 0.5
+
+var peer_id = -1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	
+		
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion: 
+		$Pivot.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -MOUSE_RANGE, MOUSE_RANGE)
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	if Input.is_action_just_pressed("Shoot"):
+		$Pivot/Blaster.shoot()
+			
+		
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -29,3 +42,9 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	rpc("_set_position", global_position)
+	rpc("_set_rotation", rotation.y)
+
+func die():
+	rpc("_die")
+	queue_free()
